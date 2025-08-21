0453c01839189ef089b4f16d33cdf33a036d94c8
DikhaAG <dikhaagsfrdy@gmail.com>
2025-08-22 01:24:35 +0700

    data for lomba web design page

--- src/data/home/web-design/header-data.ts
+++ src/data/home/web-design/header-data.ts
@@ -1,3 +1,20 @@
 import { HomeAcaraHeaderDataType } from "@/types/home/home/acara/header-data-type";
 
-// export const homeWebDesignHeaderData: HomeAcaraHeaderDataType = {};
+export const homeWebDesignHeaderData: HomeAcaraHeaderDataType = {
+        title: "Lomba Web Design (UI/UX)",
+        desc: "",
+        img: "uiux.png",
+        htm: "35.000",
+        cp: [
+                {
+                        nama: "Afi",
+                        kontak: "0897-8737-184",
+                },
+        ],
+        pembayaran: [
+                {
+                        nama: "",
+                        nomor: "",
+                },
+        ],
+};
--- src/data/home/web-design/timeline-data.ts
+++ src/data/home/web-design/timeline-data.ts
@@ -1,3 +1,12 @@
 import { HomeAcaraTimelineDataType } from "@/types/home/home/acara/timeline-data-type";
 
-export const homeWebDesignTimelineData: HomeAcaraTimelineDataType[] = [];
+export const homeWebDesignTimelineData: HomeAcaraTimelineDataType[] = [
+        { nama: "Pendaftaran", tanggal: "1 September - 3 Oktober 2025" },
+        { nama: "Pengumpulan Karya", tanggal: "8 - 15 Oktober 2025" },
+        { nama: "Seleksi Karya", tanggal: "15 - 17 Oktober 2025" },
+        { nama: "Pengumuman Finalis", tanggal: "18 Oktober 2025" },
+        {
+                nama: "Presentasi & Pengumuman Pemenang",
+                tanggal: "25 Oktober 2025",
+        },
+];
