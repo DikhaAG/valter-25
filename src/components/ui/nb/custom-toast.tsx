d7aa02a89d378dabc0dfe4522ed9a331916de67b
DikhaAG <dikhaagsfrdy@gmail.com>
2025-08-21 19:39:54 +0700

    ui components

--- src/components/ui/nb/custom-toast.tsx
+++ src/components/ui/nb/custom-toast.tsx
@@ -7,18 +7,47 @@ interface Props {
 }
 export function CustomToast({ variant = "default", message }: Props) {
         toast.custom(() => (
-                <div
-                        className={`${
-                                variant === "default" && "bg-background"
-                        } ${variant === "success" && "bg-green-400"} ${
-                                variant === "error" && "bg-red-400"
-                        } ${variant === "warning" && "bg-yellow-100"} 
-                        flex flex-row items-center gap-4 border-4 border-foreground rounded-md shadow-[7px_7px_0px_#00000040] p-4 `}
-                >
-                        {variant === "warning" && <TriangleAlert />}
-                        {variant === "error" && <CircleX />}
-                        {variant === "success" && <CircleCheck />}
-                        {message}
+                <div className="relative border-5 border-foreground rounded-xl shadow-[7px_7px_0px_#00000040] ">
+                        <div
+                                className={`${
+                                        variant === "default" && "bg-background"
+                                } ${variant === "success" && "bg-green-200"} ${
+                                        variant === "error" && "bg-red-300"
+                                } ${variant === "warning" && "bg-yellow-100"} 
+                        flex flex-row items-center gap-4 border-5 border-foreground rounded-xl shadow-[7px_7px_0px_#00000040] py-2 px-4 md:px-6`}
+                        >
+                                {variant === "warning" && <TriangleAlert />}
+                                {variant === "error" && <CircleX />}
+                                {variant === "success" && <CircleCheck />}
+                                <div className="pl-2 md:pl-4">
+                                        <div className="">
+                                                <h2 className="font-bold">
+                                                        {variant ===
+                                                                "warning" &&
+                                                                "Tunggu sebentar!"}
+                                                        {variant === "error" &&
+                                                                "Oopss!..."}
+                                                        {variant ===
+                                                                "success" &&
+                                                                "Kerja bagus!"}
+                                                </h2>
+                                        </div>
+                                        <div className="">{message}</div>
+                                </div>
+                        </div>
+                        <div
+                                className={`
+                                                absolute 
+                                               w-15
+                                               h-5
+                                                bottom-1
+                                                right-2
+                                                bg-foreground
+                                                skew-x-30
+                                                skew-y-25
+                                                -z-10
+                                        `}
+                        ></div>
                 </div>
         ));
 }
