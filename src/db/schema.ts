7a4bbff37d1c6f3c923826e168ef77807330a6f4
DikhaAG <dikhaagsfrdy@gmail.com>
2025-08-21 19:40:27 +0700

    drizzle schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,24 +1,40 @@
-import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { pgTable, uuid, text, boolean, timestamp } from "drizzle-orm/pg-core";
 
 export const timML = pgTable("tim_ml", {
-  id: uuid("id").primaryKey(),
-  namaTim: text("nama_tim").unique().notNull(),
-  noWa: text("no_wa"),
-  instansi: text("instansi"),
-  buktiPembayaran: text("bukti_pembayaran").unique(),
+        id: uuid("id").primaryKey(),
+        namaTim: text("nama_tim").unique().notNull(),
+        noWa: text("no_wa"),
+        instansi: text("instansi"),
+        buktiPembayaran: text("bukti_pembayaran").unique(),
+        statusPembayaran: boolean("status_pembayaran").default(false).notNull(),
+        createdat: timestamp("created_at", { mode: "string" })
+                .defaultNow()
+                .notNull(),
+        updatedat: timestamp("updated_at", { mode: "string" })
+                .defaultNow()
+                .notNull()
+                .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
 });
 
 export const pesertaML = pgTable("peserta_ml", {
-  id: uuid("id").primaryKey(),
-  namaTim: text("nama_tim")
-    .notNull()
-    .references(() => timML.namaTim, { onDelete: "cascade" }),
-  idML: text("id_ml").unique().notNull(),
-  npm: text("npm"),
-  nama: text("nama"),
+        id: uuid("id").primaryKey(),
+        namaTim: text("nama_tim")
+                .notNull()
+                .references(() => timML.namaTim, { onDelete: "cascade" }),
+        idML: text("id_ml").unique().notNull(),
+        npm: text("npm").unique().notNull(),
+        nama: text("nama"),
+        createdat: timestamp("created_at", { mode: "string" })
+                .defaultNow()
+                .notNull(),
+        updatedat: timestamp("updated_at", { mode: "string" })
+                .defaultNow()
+                .notNull()
+                .$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
 });
 
 export const schema = {
-          timML,
-          pesertaML
-}
+        timML,
+        pesertaML,
+};
