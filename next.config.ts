467b1979e83e229e245da005c11812f20050555c
DikhaAG <dikhaagsfrdy@gmail.com>
2025-08-21 19:41:38 +0700

    others

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
         /* config options here */
         images: {
                 domains: ["res.cloudinary.com"],
+                formats: ["image/avif", "image/webp"], // Tambahkan format yang Anda butuhkan
         },
 };
 
