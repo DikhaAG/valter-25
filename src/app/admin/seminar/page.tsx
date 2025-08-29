cd7ff9f2ce4791468ac9c3cbaaaef7cf2939ab52
DikhaAG <dikhaagsfrdy@gmail.com>
2025-08-29 16:44:39 +0700

    halaman admin seminar

---
+++ src/app/admin/seminar/page.tsx
@@ -0,0 +1,38 @@
+"use server";
+import { SeminarDataTable } from "@/components/admin/seminar/data-table";
+import { SectionCards } from "@/components/section-cards";
+
+import { getSeminarIncome } from "@/server/services/admin/getIncome";
+import {
+  getTotalPesertaSeminarBelumTerkonfirmasi,
+  getTotalPesertaSeminarTerkonfirmasi,
+} from "@/server/services/admin/getTotalPeserta";
+import { getAllClassRegistration } from "@/server/actions/queries/seminar";
+export default async function Page() {
+  const countIncome = await getSeminarIncome();
+  const countTotalSemuaPendaftarTerkonfirmasi =
+    await getTotalPesertaSeminarTerkonfirmasi();
+  const countTotalSemuaPendaftarBelumTerkonfirmasi =
+    await getTotalPesertaSeminarBelumTerkonfirmasi();
+  const countTotalSemuaPendaftar =
+    (countTotalSemuaPendaftarTerkonfirmasi.data ?? 0) +
+    (countTotalSemuaPendaftarBelumTerkonfirmasi.data ?? 0);
+
+  const classRegistions = await getAllClassRegistration();
+  return (
+    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+      <div className="font-extrabold text-2xl mx-4 md:mx-6">Seminar</div>
+      <SectionCards
+        income={countIncome.data ?? 0}
+        totalPendaftar={countTotalSemuaPendaftar}
+        totalPendaftarTerkonfirmasi={
+          countTotalSemuaPendaftarTerkonfirmasi.data ?? 0
+        }
+        totalPendaftarBelumTerkonfirmasi={
+          countTotalSemuaPendaftarBelumTerkonfirmasi.data ?? 0
+        }
+      />
+      <SeminarDataTable data={classRegistions.data!} />
+    </div>
+  );
+}
