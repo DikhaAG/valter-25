ac6cdd82fd04783d2af01801c229f09d0c5d29dc
DikhaAG <dikhaagsfrdy@gmail.com>
2025-09-01 00:03:16 +0700

    .

--- src/components/admin/app-sidebar.tsx
+++ src/components/admin/app-sidebar.tsx
@@ -64,7 +64,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={appSidebarData.user} />
+        <NavUser />
       </SidebarFooter>
     </Sidebar>
   )
