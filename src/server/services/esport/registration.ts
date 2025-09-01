984221bf8f0c7de127677c1d90295dd45983cf8d
DikhaAG <dikhaagsfrdy@gmail.com>
2025-09-02 04:47:32 +0700

    .

--- src/server/services/esport/registration.ts
+++ src/server/services/esport/registration.ts
@@ -19,104 +19,104 @@ import { isUniqueConstraintViolationError } from "@/utils/is-unique-constraint-e
 import { DrizzleQueryError, eq } from "drizzle-orm";
 import { ServerResponseType } from "@/types/server-response";
 import {
-   pesertaEsportTable,
-   timEsportTable,
+  pesertaEsportTable,
+  timEsportTable,
 } from "@/server/db/schemas/esport-schema";
 import { uploadToCloudinary } from "@/server/services/upload-to-cloudinary";
 import {
-   ParticipantAsMahasiswa,
-   teamAsGeneral,
-   TeamAsGeneral,
-   teamAsMahasiswa,
-   TeamAsMahasiswa,
+  ParticipantAsMahasiswa,
+  teamAsGeneral,
+  TeamAsGeneral,
+  teamAsMahasiswa,
+  TeamAsMahasiswa,
 } from "@/models/esport/registration-form";
 import { db } from "@/lib/drizzle";
 
 export async function registration({
-   data,
+  data,
 }: {
-   data: TeamAsMahasiswa | TeamAsGeneral;
+  data: TeamAsMahasiswa | TeamAsGeneral;
 }): Promise<ServerResponseType<string>> {
-   let result: TeamAsMahasiswa | TeamAsGeneral;
-   const { as } = data;
-   try {
-      if (as === "mahasiswa") {
-         result = await teamAsMahasiswa.parseAsync(data);
-      } else if (as === "umum") {
-         result = await teamAsGeneral.parseAsync(data);
+  let result: TeamAsMahasiswa | TeamAsGeneral;
+  const { as } = data;
+  try {
+    if (as === "mahasiswa") {
+      result = await teamAsMahasiswa.parseAsync(data);
+    } else if (as === "umum") {
+      result = await teamAsGeneral.parseAsync(data);
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: "Data yang diberikan tidak valid!.",
+      error: error,
+    };
+  }
+  if (!result!) {
+    return {
+      success: false,
+      message: "Data yang diberikan tidak valid!.",
+    };
+  }
+
+  try {
+    const buktiPembayaranUrl = await uploadToCloudinary(
+      result.buktiPembayaran,
+      result.buktiPembayaran.name
+    );
+
+    // Insert ke tabel timML
+    const insertTim = await db
+      .insert(timEsportTable)
+      .values({
+        id: uuidv4(),
+        as,
+        namaTim: result.namaTim,
+        noWa: result.noWa,
+        instansi: result.instansi,
+        buktiPembayaran: buktiPembayaranUrl,
+      })
+      .returning({ insertedId: timEsportTable.id });
+
+    // Setelah berhasil, insert ke tabel pesertaML
+    const pesertaToInsert = result.peserta.map((p) => ({
+      id: uuidv4(),
+      namaTim: result.namaTim,
+      idML: p.idML,
+      nama: p.nama,
+      npm: (p as ParticipantAsMahasiswa).npm,
+    }));
+
+    await db.insert(pesertaEsportTable).values(pesertaToInsert);
+
+    return {
+      success: true,
+      data: insertTim[0].insertedId,
+    };
+  } catch (error) {
+    await db
+      .delete(timEsportTable)
+      .where(eq(timEsportTable.namaTim, result.namaTim));
+    if (isUniqueConstraintViolationError(error)) {
+      if ((error as DrizzleQueryError).cause?.message.includes("id_ml")) {
+        return {
+          success: false,
+          message: "ID telah terdaftar.",
+        };
+      } else if (
+        (error as DrizzleQueryError).cause?.message.includes("npm")
+      ) {
+        return {
+          success: false,
+          message: "NPM telah terdaftar.",
+        };
       }
-   } catch (error) {
-      return {
-         success: false,
-         message: "Data yang diberikan tidak valid!.",
-         error: error,
-      };
-   }
-   if (!result!) {
-      return {
-         success: false,
-         message: "Data yang diberikan tidak valid!.",
-      };
-   }
-
-   try {
-      const buktiPembayaranUrl = await uploadToCloudinary(
-         result.buktiPembayaran,
-         result.buktiPembayaran.name
-      );
-
-      // Insert ke tabel timML
-      const insertTim = await db
-         .insert(timEsportTable)
-         .values({
-            id: uuidv4(),
-            as,
-            namaTim: result.namaTim,
-            noWa: result.noWa,
-            instansi: result.instansi,
-            buktiPembayaran: buktiPembayaranUrl,
-         })
-         .returning({ insertedId: timEsportTable.id });
-
-      // Setelah berhasil, insert ke tabel pesertaML
-      const pesertaToInsert = result.peserta.map((p) => ({
-         id: uuidv4(),
-         namaTim: result.namaTim,
-         idML: p.idML,
-         nama: p.nama,
-         npm: (p as ParticipantAsMahasiswa).npm,
-      }));
-
-      await db.insert(pesertaEsportTable).values(pesertaToInsert);
-
-      return {
-         success: true,
-         data: insertTim[0].insertedId,
-      };
-   } catch (error) {
-      await db
-         .delete(timEsportTable)
-         .where(eq(timEsportTable.namaTim, result.namaTim));
-      if (isUniqueConstraintViolationError(error)) {
-         if ((error as DrizzleQueryError).cause?.message.includes("id_ml")) {
-            return {
-               success: false,
-               message: "ID telah terdaftar.",
-            };
-         } else if (
-            (error as DrizzleQueryError).cause?.message.includes("npm")
-         ) {
-            return {
-               success: false,
-               message: "NPM telah terdaftar.",
-            };
-         }
-      }
-      return {
-         success: false,
-         statusCode: 500,
-         error: error,
-         message: "Gagal memproses permintaan. Periksa log server.",
-      };
-   }
+    }
+    return {
+      success: false,
+      statusCode: 500,
+      error: error,
+      message: "Gagal memproses permintaan. Periksa log server.",
+    };
+  }
 }
