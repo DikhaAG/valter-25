da160ba827f8be70d90eb775a2d7ac2985280dff
DikhaAG <dikhaagsfrdy@gmail.com>
2025-09-01 14:05:15 +0700

    revisi

--- src/data/home/pelatihan/timeline-data.ts
+++ src/data/home/pelatihan/timeline-data.ts
@@ -2,5 +2,5 @@ import { TimelineDataType } from "@/types/timeline";
 
 export const timelineData: TimelineDataType[] = [
    { nama: "Registrasi Peserta", tanggal: "1 September - 12 Oktober 2025" },
-   { nama: "Pelaksanaan", tanggal: "20, 21, 22 Oktober 2025" },
+   { nama: "Pelaksanaan", tanggal: "20 s.d. 22 Oktober 2025" },
 ];
--- src/data/home/seminar/timeline-data.ts
+++ src/data/home/seminar/timeline-data.ts
@@ -2,4 +2,5 @@ import { TimelineDataType } from "@/types/timeline";
 
 export const timelineData: TimelineDataType[] = [
    { nama: "Pendaftaran", tanggal: "1 September - 22 Oktober 2022" },
+   { nama: "Pelaksanaan", tanggal: "30 Oktober 2022" },
 ];
